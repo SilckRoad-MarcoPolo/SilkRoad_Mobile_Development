827ad6206684add1518924c6854c0ad6d9294806
Amira shawki <amirashawki7@gmail.com>
2024-09-18 23:06:04 +0300

    feat : add ui of home screen

---
+++ lib/features/home/widgets/trending_course_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:silk_road/features/trending_courses/view/trending_courses_card.dart';
+
+class TrendingCourseList extends StatelessWidget {
+  TrendingCourseList({super.key});
+    final List<Map<String, dynamic>> courses = [
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },
+    {
+      'name': 'Data Analysis',
+      'instructor': 'Jhon Doe',
+      'hours': '37/40 hrs',
+      'progress': 80,
+      'image': 'assets/data_analysis.png',
+    },];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+       shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: courses.length,
+      itemBuilder: (BuildContext,index){
+      var course = courses[index];
+      return TrendingCoursesCard(course: course);
+    });
+  }
+}
