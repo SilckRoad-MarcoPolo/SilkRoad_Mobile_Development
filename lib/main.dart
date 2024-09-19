c3fcf59e7141c90ec4ffa98323af2bc807193539
yshedid <yosef.shedid.prof@gmail.com>
2024-09-19 18:20:06 +0300

    finished ui

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:silk_road/features/login/view/login_page.dart';
 import 'package:silk_road/features/on_boarding/view/on_boarding.dart';
 import 'package:silk_road/features/reset_password/view/forgot_password.dart';
 import 'package:silk_road/features/reset_password/view/new_password.dart';
+import 'features/appointments/view/appointments.dart';
 import 'features/reset_password/view/otp_verification.dart';
 import 'features/trending_courses/view/trending_courses.dart';
 
@@ -30,6 +31,6 @@ class SilkRoad extends StatelessWidget {
             ),
         debugShowCheckedModeBanner: false,
 
-        home:BOttomNavigationBar() ,);
+        home:Appointments() ,);
   }
 }
